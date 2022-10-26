38f05db8e45a91ec9323215c1a29a5ed6ba4eeaf
BalagwisJohnDenver <imbajhey@gmail.com>
2022-10-26 10:57:09 +0800

    commit

--- src/App.java
+++ src/App.java
@@ -15,23 +15,63 @@ public class App {
     }
 
     // create a method with parameters
-    public int sum3(int a, int b) {
+    public int sum3(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // method overloading with different parameters using float
+    public float sum3(float a, int b) {
         return a + b;
     }
 
-    // method overloading
-    public float sum3(float a, int b) {
+    public byte sum3(byte a, int b) {
+        return (byte) (a + b);
+    }
+
+    // method overloading with parameters using double data type
+    public double sum3(double a, double b) {
         return a + b;
+    }
+
+    // method overloading with parameters using byte data type
+    public byte sum4(byte a, byte b) {
+        return (byte) (a + b);
+    }
+
+    // method overloading with parameters using long data type
+    public long sum5(long a, long b) {
+        return a + b;
+    }
+
+    // method overloading with parameters using short data type
+    public short sum6(short a, short b) {
+        return (short) (a + b);
     }
 
     // main method
     public static void main(String[] args) throws Exception {
 
         App app = new App();
+
         app.sum();
+
         System.out.println("The sum of method with return type is: " + app.sum2());
-        System.out.println("The sum of method with parameters is: " + app.sum3(100, 3));
-        System.out.println("The sum of method with parameters is: " + app.sum3(100.5f, 3));
+
+        System.out.println("The sum of method with parameters is: " + app.sum3(1, 2, 3));
+
+        System.out.println("The sum of method with parameters using float is: " + app.sum3(2.5f, 3));
+
+        System.out.println("The sum of method with parameters using byte is: " + app.sum3((byte) 2, 3));
+
+        System.out.println("The sum of method with parameters using double data type is: " + app.sum3(100.0, 3.0));
+
+        System.out.println(
+                "The sum of method with parameters using byte data type is: " + app.sum4((byte) 100, (byte) 3));
+
+        System.out.println("The sum of method with parameters using long data type is: " + app.sum5(100L, 3L));
+
+        System.out.println(
+                "The sum of method with parameters using short data type is: " + app.sum6((short) 100, (short) 3));
 
     }
 
